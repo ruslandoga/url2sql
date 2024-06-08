ff4e961d277bd65f4f9d7e659cb2643f078dcc70
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-06-08 13:24:18 +0700

    init

---
+++ lib/url2sql.ex
@@ -0,0 +1,18 @@
+defmodule URL2SQL do
+  @moduledoc """
+  Documentation for `URL2SQL`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> URL2SQL.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/url2sql_test.exs
@@ -0,0 +1,8 @@
+defmodule URL2SQLTest do
+  use ExUnit.Case
+  doctest URL2SQL
+
+  test "greets the world" do
+    assert URL2SQL.hello() == :world
+  end
+end
